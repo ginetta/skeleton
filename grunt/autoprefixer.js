2b3919e8a819e923efdce9e548088aebf95a512f
Joao Figueiredo <lucalanca2@hotmail.com>
2014-07-14 14:47:35 +0200

    Adds autoprefixer task.

---
+++ grunt/autoprefixer.js
@@ -0,0 +1,9 @@
+module.exports = {
+    options: {
+      browsers: ['last 2 version']
+    },
+    all: {
+        dest: '<%= config.targetDir %>/css/main.css',
+        src:  '<%= config.targetDir %>/css/main.css'
+    }
+};
