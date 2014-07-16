2206d85882e007fe55cb8e900732712b0c31bef9
David Aerne <meodai@gmail.com>
2014-07-17 01:44:36 +0200

    updates sensible, adds requestAnimationFrame AMD version

--- app/js/config-require.js
+++ app/js/config-require.js
@@ -4,7 +4,8 @@ requirejs.config({
     paths: {
         jquery: '../bower_components/jquery/dist/jquery',
         mymodule: '../modules/mymodule/mymodule',
-        mediaQuery: '../bower_components/sensible/dist/mediaQuery'
+        mediaQuery: '../bower_components/sensible/mediaQuery',
+        requestAnimationFrame: '../bower_components/requestAnimationFrame/app/requestAnimationFrame'
     },
     shim: {
 
