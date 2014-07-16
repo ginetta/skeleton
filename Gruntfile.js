10fca38ca41d4721e1eca5399734260b6d96aaa0
lejoe <ciao@lejoe.com>
2014-07-16 14:10:03 +0200

    Moving the grunt tasks folder to the lib folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,14 +8,16 @@ var config  = {
 
 module.exports = function (grunt) {
     'use strict';
+    var path = require('path');
 
     require('time-grunt')(grunt);
 
     // Loads grunt tasks found on package.json automatically
     require('load-grunt-tasks')(grunt, {pattern: 'grunt-*'});
 
-    // Loads grunt tasks' configuration in grunt/<task-name>.js
+    // Loads grunt tasks' configuration in lib/grunt-tasks/<task-name>.js
     require('load-grunt-config')(grunt, {
-        data: { config: config }
+        data: { config: config },
+        configPath: path.join(process.cwd(), 'lib/grunt-tasks')
     });
 };
