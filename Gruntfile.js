633fc614444964c40ca4147d3944ab6cbfa29d4b
Joao Figueiredo <lucalanca2@hotmail.com>
2014-07-23 11:23:00 +0200

    Adds a addComponent task for automating the process of creating a new component or sub-component.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-var config  = {
+var config = {
     srcDir: 'app',
     targetDir: 'dist',
     libDir: 'lib',
@@ -7,23 +7,34 @@ var config  = {
     assetsDir: 'assets'
 };
 
-module.exports = function (grunt) {
+module.exports = function(grunt) {
     'use strict';
     var path = require('path');
 
     require('time-grunt')(grunt);
 
     // Loads grunt tasks found on package.json automatically
-    require('load-grunt-tasks')(grunt, {pattern: 'grunt-*'});
+    require('load-grunt-tasks')(grunt, {
+        pattern: 'grunt-*'
+    });
 
     // Requiring our custom grunt tasks from the toolset
     // TODO: move to npm
-    require('./lib/toolset/grunt-tasks/assembler/pagesAssembler.js')(grunt, { config: config });
-    require('./lib/toolset/grunt-tasks/assembler/styleguideAssembler.js')(grunt, { config: config });
+    require('./lib/toolset/grunt-tasks/assembler/pagesAssembler.js')(grunt, {
+        config: config
+    });
+    require('./lib/toolset/grunt-tasks/assembler/styleguideAssembler.js')(grunt, {
+        config: config
+    });
+    require('./lib/toolset/grunt-tasks/assembler/addComponent.js')(grunt, {
+        config: config
+    });
 
     // Loads grunt tasks' configuration in lib/grunt-tasks/<task-name>.js
     require('load-grunt-config')(grunt, {
-        data: { config: config },
+        data: {
+            config: config
+        },
         configPath: path.join(process.cwd(), 'lib/grunt-tasks')
     });
 };
