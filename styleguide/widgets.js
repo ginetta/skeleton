e05528c878f0077894b4655807dccd9785e0d9a8
Joao Figueiredo <lucalanca2@hotmail.com>
2015-02-19 10:58:04 +0100

    Added widgets.js to styleguide

--- styleguide/widgets.js
+++ styleguide/widgets.js
@@ -19,7 +19,7 @@ angular
 angular.module('app.widgets.templates').run(['$templateCache', function($templateCache) {
   'use strict';
 
-  $templateCache.put('.tmp/styleguide/widgets/dist/hello-world/template.html',
+  $templateCache.put('widgets/hello-world/template.html',
     "\n" +
     "<div data-ng-class=\"{ 'is-active': options.isActive }\" class=\"hello-world\">\n" +
     "  <h1 style=\"color: {{ options.color }}\"> {{ data.greeting }}</h1>\n" +
