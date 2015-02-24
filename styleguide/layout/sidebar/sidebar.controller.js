b7e1780d35c56dcab42af8fe16830a641437ec0d
Joao Figueiredo <lucalanca2@hotmail.com>
2015-02-24 19:00:20 +0100

    Removes styleguide folder from repo tracking.

--- styleguide/layout/sidebar/sidebar.controller.js
+++
@@ -1,8 +0,0 @@
-angular.module('styleguide.layout.sidebar', [ 'styleguide.common' ])
-  .controller('SidebarCtrl', function ($scope, StyleguideData) {
-
-    StyleguideData.getComponentsData().then(function (components) {
-      console.log('components-data', components);
-      $scope.components = components;
-    })
-  });
--- styleguide/widgets/directives.js
+++
@@ -1,4 +0,0 @@
-angular
-  .module('app.widgets.templates', []);
-angular
-  .module('app.widgets', ["app.widgets.templates","app.widgets.hello-world"]);
