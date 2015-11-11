e62d22ce58269a6825669c2a2f14266f8ac349fa
lejoe <ciao@lejoe.com>
2015-11-11 13:13:30 +0100

    Removes console logs

--- gulp/utils/tasksHelpers.js
+++ gulp/utils/tasksHelpers.js
@@ -1,13 +1,11 @@
 'use strict';
 
 function TaskHelpers(gulp, config) {
-  console.log(config);
   if (this instanceof TaskHelpers === false){
     return new TaskHelpers(gulp, config);
   }
 
   this.getTask = function getTask (task) {
-    console.log(this.config);
     return require('../tasks/' + task)(this.gulp, this.$, this.config);
   };
 
