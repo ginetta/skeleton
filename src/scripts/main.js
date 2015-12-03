a09b9a69db1e19d1959b3edbe87aa1b4170c538d
lejoe <ciao@lejoe.com>
2015-12-03 10:32:53 +0100

    Removes jquery as default
    Adds sensible/mediaquery as the default example.
    Which also contains jquery but it you remove it, jquery will also be removed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,11 @@ require('elements/header/script');
 
 // This is how you require a component from node_modules
 // Make sure to add it to the vendors in /src/scripts/vendor.js
-var $ = require('jquery');
+var mediaQuery = require('sensible/mediaQuery');
 
 // The code for the website comes here.
+if ( mediaQuery.isNot('mobile') ){
+  console.log("I'm a not a mobile")
+}
+
+
