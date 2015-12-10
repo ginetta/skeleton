ba2d44e47f451d996ecb6924931c6a93ae30ffe7
lejoe <ciao@lejoe.com>
2015-12-10 12:18:42 +0100

    Refactors the release task
    Asks for the bumpt type
    creates a new version
    tags it
    pushes to github

--- gulp/tasks/release.js
+++ gulp/tasks/release.js
@@ -3,59 +3,62 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var logger = require('gulplog');
-var releaseBranch = 'master';
-var releaseCommitMessage = 'New release version';
+var inquirer = require('inquirer');
+var exec = require('child_process').spawn;
+
+var allowedBranch = 'master';
+var bumpTypeQuestion =  {
+  type: 'list',
+  name: 'bump',
+  message: 'What type of bump would you like to do?',
+  default: 'minor',
+  choices: ['patch', 'minor', 'major']
+};
 
 // Makes sure we do this on the master branch
-var verifyBranch = function() {
+var asserBranchAllowed = function(done) {
   $.git.revParse({args:'--abbrev-ref HEAD', quiet: true}, function(err, currentBranch)  {
-    if (currentBranch != releaseBranch) {
-      logger.error('ERR: You need to be on the %s branch to be able to deploy', releaseBranch);
-      process.exit(0);
-    }
-  })
-}
-verifyBranch.displayName = 'Verify branch';
+    // if (currentBranch != allowedBranch) {
+    //   logger.error('ERR: You need to be on the %s branch to be able to deploy', allowedBranch);
+    //   process.exit(1);
+    // }
+    done();
+  });
 
-// Defaults to minor for now (better than no version bump)
-// TODO:
-// - get the bumptype from params
-// - make that a requirement (no default)
-var getBumpType = function() {
-  // var argv = require('yargs')
-  //           .demand('bumpType', 'Usage: --bumpType={patch, minor, major}').argv;
-  return 'minor'
 }
+asserBranchAllowed.displayName = 'Verify branch';
+
+// Asks for the bump type
+var askBumpType = function(done) {
+  inquirer.prompt([bumpTypeQuestion], function( answers ) {
+    if (answers !== null && answers[bumpTypeQuestion.name] !== null) {
+      try {
+        bumpVersion(answers[bumpTypeQuestion.name], done);
+      } catch (e) {
+        done();
+      }
+
+    } else {
+      logger.error('ERR: There was an error with the provided bump type');
+      done();
+    }
+  });
+}
+askBumpType.displayName = 'Ask bump type';
+
+// Bumps the version of the project
+var bumpVersion = function(bumpType, cb) {
+  var options = { stdio: ['ignore', 'ignore',process.stderr], cwd: process.cwd()}
+  var p = exec('npm', ['version', bumpType], options);
+  p.on('close', cb);
+}
+bumpVersion.displayName = 'Bump Version';
 
 // Bumps the version on package.json and commits it locally and pushes to master
-var bumpVersion = function() {
-  return gulp.src('./package.json')
-    .pipe($.bump({type: getBumpType()}))
-    .pipe(gulp.dest('./'))
-    // .pipe($.git.add())
-    // .pipe($.git.commit(releaseCommitMessage))
+var releaseTask = function(done) {
+  return new Promise(gulp.series(asserBranchAllowed, askBumpType));
 }
-bumpVersion.displayName = 'Bump version';
-
-// Merges to the releases branch, tags the version on git and pushes it to github
-var publishRelease = function(done) {
-  done();
-}
-publishRelease.displayName = 'Publish release';
-
-// Commits the build folder to another branch and pushes it to github
-var publishBuild = function(done) {
-  done();
-}
-publishBuild.displayName = 'Publish build release';
-
-
-var releaseTask = function () {
-  verifyBranch();
-  return new Promise(gulp.series(bumpVersion, publishRelease, publishBuild));
-}
-
-releaseTask.description = 'Makes a new release of the website';
+releaseTask.description = 'Bumps the npm version';
 
 module.exports = function(gulp, $, config) {
   return releaseTask;
