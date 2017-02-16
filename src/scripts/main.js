6460e16e4b8ea75ec606fc320b6bbd37dbbafa22
João Figueiredo <lucalanca2@hotmail.com>
2017-02-16 16:21:23 +0100

    fix lint with airbnb-base rules.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,11 @@
-'use strict';
+// This is how you require a component from node_modules
+// Make sure to add it to the vendors in /src/scripts/vendor.js
+const mediaQuery = require('sensible/mediaQuery');
 
 // This is how you require your component script:
 require('elements/header/script');
 
-// This is how you require a component from node_modules
-// Make sure to add it to the vendors in /src/scripts/vendor.js
-var mediaQuery = require('sensible/mediaQuery');
-
 // The code for the website comes here.
-if ( mediaQuery.isNot('mobile') ){
-  console.log("I'm a not a mobile")
+if (mediaQuery.isNot('mobile')) {
+  console.log("I'm a not a mobile");
 }
-
-
