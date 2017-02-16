3f4504f140f7331a817d58c33c439884ada3751b
João Figueiredo <lucalanca2@hotmail.com>
2017-02-16 16:36:00 +0100

    Adds babel loader but moves configuration to its own file.

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,18 @@ module.exports = config => {
       path: path.resolve(__dirname, 'build')
     },
 
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            'babel-loader'
+          ]
+        }
+      ],
+    },
+
 
     // options for resolving module requests
     resolve: {
