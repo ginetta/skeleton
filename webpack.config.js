55888bccf31fb69af3e94d0305ae8ac17d981905
João Figueiredo <lucalanca2@hotmail.com>
2017-02-17 16:27:18 +0100

    Fixes linting.

--- webpack.config.js
+++ webpack.config.js
@@ -58,24 +58,24 @@ module.exports = config => ({
   // See: https://webpack.js.org/guides/production-build/#the-manual-way-configuring-webpack-for-multiple-environments
   ].concat(config.isProd ? [
     new webpack.LoaderOptionsPlugin({
-      minimize : true,
-      debug    : false,
+      minimize: true,
+      debug: false,
     }),
     new webpack.DefinePlugin({
-      'process.env' : {
-        NODE_ENV : JSON.stringify('production'),
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
       },
     }),
     new webpack.optimize.UglifyJsPlugin({
-      beautify : false,
-      mangle   : {
-        screw_ie8   : true,
-        keep_fnames : true,
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true,
       },
-      compress : {
-        screw_ie8 : true,
+      compress: {
+        screw_ie8: true,
       },
-      comments : false,
+      comments: false,
     }),
   ] : []),
 });
