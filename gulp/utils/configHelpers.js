bc97cb883b36aaba1e8781ba3a2094f13e63171d
João Figueiredo <lucalanca2@hotmail.com>
2017-03-01 10:30:08 +0100

    simplify configHelpers getAvailableLanguages

--- gulp/utils/configHelpers.js
+++ gulp/utils/configHelpers.js
@@ -2,14 +2,8 @@ const glob = require('glob');
 
 module.exports = {
   getAvailableLanguages: (config) => {
-    const entry = config.entryGlobs.content;
     const entryPath = config.entryPaths.content;
-    return entry.map(e => glob.sync(e))
-      .reduce((a, b) => a.concat(b), [])
-      .map((path) => {
-        const withoutBase = path.replace(entryPath, '');
-        return withoutBase.slice(0, withoutBase.indexOf('/'));
-      });
+    return glob.sync('*', { cwd: entryPath });
   },
 
   // Put the default language at the root
