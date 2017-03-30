c52c7ae17f6bc6aa2c5580a50cedaf3c84a54bb9
João Figueiredo <lucalanca2@hotmail.com>
2017-03-30 10:51:20 +0200

    Meta files now can be hidden files or directories.

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -14,7 +14,7 @@ module.exports = (gulp, $, config) => {
     gulp.watch(config.watchGlobs.pages, gulp.series('build:pages', reload));
     gulp.watch(config.watchGlobs.content, gulp.series('build:content', 'build:pages', reload));
     gulp.watch(config.watchGlobs.assets, gulp.parallel('build:assets'));
-    gulp.watch(config.watchGlobs.meta, gulp.parallel('build:meta'));
+    gulp.watch(config.watchGlobs.meta, { dot: true }, gulp.parallel('build:meta'));
     gulp.watch(config.watchGlobs.icons, gulp.parallel('build:icons-sprite'));
   };
 
